36b11429a0920efdc3d60a459e1c749ad0e37f4b
khuongsatou <yoshi240499@gmail.com>
2019-10-26 02:55:50 +0700

    basic masterpage

---
+++ MasterPageASPver3/MasterPageASPver3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MasterPageASPver3.Startup))]
+namespace MasterPageASPver3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
